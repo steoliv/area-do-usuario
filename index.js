3024431fdfb6ce3027c9ae4482246056b81e0794
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-11-23 21:40:39 -0300

    Inserindo evento no btn do formulário

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user ={}; //json
 
-fields.forEach(function(field, index){
 
-    if(field.name == "gender"){
 
-        if(field.checked){
+//Evento click nos btns
 
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+
+    event.preventDefault(); //Cancela o comando padrão que o evento teria
+    
+    fields.forEach(function(field, index){
+    
+        if(field.name == "gender"){
+    
+            if(field.checked){
+    
+                user[field.name] = field.value;
+    
+            }
+            
+        }else{
+    
             user[field.name] = field.value;
-
-        }
         
-    }else{
-
-        user[field.name] = field.value;
+        }
     
-    }
-
+        
+    });
     
-});
+    console.table(user); // exibindo JSON
 
 
+    });
 
-
-console.log(user); // exibindo JSON
